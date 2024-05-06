7b1eadb84ce90aed4ab62e1def018982ea7b0174
NeoUmSpark <168011761+NeoUmSpark@users.noreply.github.com>
2024-05-06 20:34:28 +0300

    hometask_for_cars_list

---
+++ homework_m2_for.py
@@ -0,0 +1,8 @@
+cars_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(len(cars_)):
+    cars_count += 10
+    print('Я езжу на', f'{cars_count}', 'автомобилях марки', cars_[i])
+
+# честно говоря, не особо понял, что требуется в задании
+# зато попробовал сделать красиво :)
