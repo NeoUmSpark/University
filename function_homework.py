04d1c790aa697c90cc5eaee0a565917000a984b5
NeoUmSpark <168011761+NeoUmSpark@users.noreply.github.com>
2024-05-08 15:50:00 +0300

    def_homework

---
+++ function_homework.py
@@ -0,0 +1,9 @@
+def print_params(number):
+    while number > 1:
+        print("Print number", number)
+        number -= 1
+        print("New number", number)
+
+
+print_params(3)
+print_params(2)
