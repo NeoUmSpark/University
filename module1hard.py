c7dfff0de98277fc0feb807eb5b94d67d9c93416
NeoUmSpark <168011761+NeoUmSpark@users.noreply.github.com>
2024-05-08 13:40:56 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -1,14 +1,12 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-studentsnew = list(students)  # перевел множество учеников в список для сортировки
-studentsnew.sort()
 
-avg_grades = [sum(grades[0]) / len(grades[0]),
-      sum(grades[1]) / len(grades[1]),
-      sum(grades[2]) / len(grades[2]),
-      sum(grades[3]) / len(grades[3]),
-      sum(grades[4]) / len(grades[4])]  # вычислил средний балл
+students_new = list(students)  # перевел множество учеников в список для сортировки
+students_new.sort()  # сортировка по алфавиту
 
-avg_grades_for_students = dict(zip(studentsnew, avg_grades))  # функция zip
-# поэлементно сгруппировала объекты в указаных списках
+avg_grades_for_students = {}  # пустой словарь для внесения учеников с оценками
+
+for i, student in enumerate(students_new):
+    avg_grades_for_students[student] = sum(grades[i]) / len(grades[i])
+# в цикле перебрал учеников (student) по алфавиту со средним арифметическим от оценок (i)
 print("Средний балл учеников: ", avg_grades_for_students)
