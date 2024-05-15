3c73025b7f62dab3071eed08b2c18d953792ed9d
NeoUmSpark <168011761+NeoUmSpark@users.noreply.github.com>
2024-05-15 22:40:20 +0300

    Update homework_m3.py

--- homework_m3.py
+++ homework_m3.py
@@ -1,11 +1,10 @@
-def test(a = "param", b = 54):
+def test():
+    a = "param"
+    b = 54
     print(a, b)
 
 
-def test2():
-    a = "hello,"
-    b = "this is module number"
-    c = 3
+def test2(a="hello", b="this is module", c=3):
     print(a, b, c)
 
 
