149a17673f5d1709d48968fc1c9bf49ec839c2d0
NeoUmSpark <168011761+NeoUmSpark@users.noreply.github.com>
2024-08-05 21:32:43 +0300

    Update 7_3_1.py

--- 7_3_1.py
+++ 7_3_1.py
@@ -1,9 +1,12 @@
 import string
 
+
 class WordsFinder:
     def __init__(self, *file_names):
         self.names = list(file_names)
         self.all_words = {}
+
+    
     def get_all_words(self):
         for file_name in self.names:
             with open(file_name, encoding='utf-8') as file:
@@ -11,6 +14,8 @@ class WordsFinder:
                 words = text.lower().translate(str.maketrans('', '', string.punctuation)).split()
                 self.all_words[file_name] = words
         return self.all_words
+
+    
     def find(self, word):
         result = {}
         for name, words in self.get_all_words().items():
@@ -19,6 +24,7 @@ class WordsFinder:
                     result[name] = i +1
                     return result
 
+    
     def count(self, word):
         result = {}
         for name, words in self.get_all_words().items():
